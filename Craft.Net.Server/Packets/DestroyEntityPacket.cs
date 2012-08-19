64e784405887db554acf2e9385b5e263085a7946
Drew DeVault <sir@cmpwn.com>
2012-08-19 02:06:24 -0600

    Added missing file

---
+++ Craft.Net.Server/Packets/DestroyEntityPacket.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Craft.Net.Data;
+using Craft.Net.Data.Entities;
+
+namespace Craft.Net.Server.Packets
+{
+    public class DestroyEntityPacket : Packet
+    {
+        public int[] EntityIds;
+
+        public DestroyEntityPacket()
+        {
+        }
+
+        public DestroyEntityPacket(params int[] entities) // TODO: I don't really like this
+        {
+            EntityIds = entities;
+        }
+
+        public override byte PacketId
+        {
+            get { return 0x1D; }
+        }
+
+        public override int TryReadPacket(byte[] buffer, int length)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public override void HandlePacket(MinecraftServer server, ref MinecraftClient client)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public override void SendPacket(MinecraftServer server, MinecraftClient client)
+        {
+            byte[] payload = new byte[] { PacketId, (byte)EntityIds.Length };
+            foreach (int id in EntityIds)
+                payload = payload.Concat(DataUtility.CreateInt32(id)).ToArray();
+            client.SendData(payload);
+        }
+    }
+}
