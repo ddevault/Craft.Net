52048056dfc6b7f9ae1f10aef003b94c36278422
Drew DeVault <sir@cmpwn.com>
2012-08-20 01:59:20 -0600

    Added basic unit tests

---
+++ Craft.Net.Data.Unit/ItemTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Craft.Net.Data.Blocks;
+using Craft.Net.Data.Generation;
+using Craft.Net.Data.Items;
+using NUnit.Framework;
+
+namespace Craft.Net.Data.Unit
+{
+    [TestFixture]
+    public class ItemTest
+    {
+        public World World;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            World = new World(new FlatlandGenerator());
+        }
+
+        [Test]
+        public void TestHoe()
+        {
+            Vector3 grassBlock = new Vector3(0, 3, 0);
+
+            var hoe = new DiamondHoeItem();
+            hoe.OnItemUsed(grassBlock, Vector3.Up, Vector3.Zero, World, null);
+
+            Assert.AreEqual(World.GetBlock(grassBlock), new FarmlandBlock());
+        }
+    }
+}
---
+++ Craft.Net.Data.Unit/WorldTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Craft.Net.Data.Blocks;
+using Craft.Net.Data.Generation;
+using NUnit.Framework;
+
+namespace Craft.Net.Data.Unit
+{
+    [TestFixture]
+    public class WorldTest
+    {
+        public World World;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            World = new World(new FlatlandGenerator());
+        }
+
+        [Test]
+        public void TestGetBlock()
+        {
+            // Test vertical sections
+            Assert.AreEqual(World.GetBlock(Vector3.Zero), new BedrockBlock());
+            Assert.AreEqual(World.GetBlock(Vector3.Up), new DirtBlock());
+            Assert.AreEqual(World.GetBlock(new Vector3(0, 3, 0)), new GrassBlock());
+
+            // Test quadrants
+            Assert.AreEqual(World.GetBlock(new Vector3(1, 0, 1)), new BedrockBlock());
+            Assert.AreEqual(World.GetBlock(new Vector3(-1, 0, 1)), new BedrockBlock());
+            Assert.AreEqual(World.GetBlock(new Vector3(1, 0, -1)), new BedrockBlock());
+            Assert.AreEqual(World.GetBlock(new Vector3(-1, 0, -1)), new BedrockBlock());
+        }
+
+        [Test]
+        public void TestSetBlock()
+        {
+            World.SetBlock(Vector3.Zero, new GoldBlock());
+            Assert.AreEqual(World.GetBlock(Vector3.Zero), new GoldBlock());
+        }
+    }
+}
