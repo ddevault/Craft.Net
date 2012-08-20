ae01618535d0869c3d854865292335f6b93b9e7a
Drew DeVault <sir@cmpwn.com>
2012-08-20 02:15:53 -0600

    Added bucket test

--- Craft.Net.Data.Unit/ItemTest.cs
+++ Craft.Net.Data.Unit/ItemTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Craft.Net.Data.Blocks;
+using Craft.Net.Data.Entities;
 using Craft.Net.Data.Generation;
 using Craft.Net.Data.Items;
 using NUnit.Framework;
@@ -12,6 +13,41 @@ namespace Craft.Net.Data.Unit
     [TestFixture]
     public class ItemTest
     {
+        [Test]
+        public void TestBuckets()
+        {
+            World world = new World(new FlatlandGenerator());
+            PlayerEntity player = new PlayerEntity()
+            {
+                GameMode = GameMode.Creative
+            };
+            Vector3 targetBlock = new Vector3(0, 3, 0);
+            Vector3 liquidBlock = targetBlock + Vector3.Up;
+            world.SetBlock(targetBlock, new AirBlock());
+
+            BucketItem bucket = new BucketItem();
+            LavaBucketItem lavaBucket = new LavaBucketItem();
+            WaterBucketItem waterBucket = new WaterBucketItem();
+
+            // TODO: Survival tests
+            waterBucket.OnItemUsed(targetBlock, Vector3.Up, Vector3.Zero, world, player);
+            Assert.AreEqual(world.GetBlock(liquidBlock), new WaterFlowingBlock());
+
+            bucket.OnItemUsed(targetBlock, Vector3.Up, Vector3.Zero, world, player);
+            Assert.AreEqual(world.GetBlock(liquidBlock), new AirBlock());
+
+            lavaBucket.OnItemUsed(targetBlock, Vector3.Up, Vector3.Zero, world, player);
+            Assert.AreEqual(world.GetBlock(liquidBlock), new LavaFlowingBlock());
+
+            bucket.OnItemUsed(targetBlock, Vector3.Up, Vector3.Zero, world, player);
+            Assert.AreEqual(world.GetBlock(liquidBlock), new AirBlock());
+
+            world.SetBlock(liquidBlock, new BedrockBlock());
+
+            bucket.OnItemUsed(targetBlock, Vector3.Up, Vector3.Zero, world, player);
+            Assert.AreEqual(world.GetBlock(liquidBlock), new BedrockBlock());
+        }
+
         [Test]
         public void TestBrewingStand()
         {
