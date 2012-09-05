cc31e3dd1d2fcb5ec8afd6cf1b260e349636244a
Drew DeVault <sir@cmpwn.com>
2012-09-04 21:27:50 -0600

    Added remaining placement items

--- Craft.Net.Data/Items/CarrotItem.cs
+++ Craft.Net.Data/Items/CarrotItem.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Craft.Net.Data.Blocks;
 
 namespace Craft.Net.Data.Items
 {
@@ -10,6 +11,12 @@ namespace Craft.Net.Data.Items
         public override ushort Id
         {
             get { return 391; }
+        }
+
+        public override void OnItemUsed(World world, Vector3 clickedBlock, Vector3 clickedSide, Vector3 cursorPosition, Entities.Entity usedBy)
+        {
+            if (world.GetBlock(clickedBlock + clickedSide) == 0)
+                world.SetBlock(clickedBlock + clickedSide, new CarrotBlock());
         }
     }
 }
