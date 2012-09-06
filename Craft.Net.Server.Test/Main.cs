add42abb7311a1ca95aa389825263ea063b0c30c
Drew DeVault <sir@cmpwn.com>
2012-09-05 19:28:27 -0600

    Uncommented delete-on-start world code

--- Craft.Net.Server.Test/Main.cs
+++ Craft.Net.Server.Test/Main.cs
@@ -30,8 +30,8 @@ namespace Craft.Net.Server.Test
             // Add a flatland world
 #if DEBUG
             // Use a fresh world each time
-            //if (Directory.Exists("world"))
-            //    Directory.Delete("world", true);
+            if (Directory.Exists("world"))
+                Directory.Delete("world", true);
 #endif
             minecraftServer.AddLevel(new Level(Path.Combine(Directory.GetCurrentDirectory(), "world")));
             minecraftServer.DefaultLevel.GameMode = GameMode.Creative;
