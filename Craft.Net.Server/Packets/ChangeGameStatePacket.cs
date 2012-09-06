d524d589b898a7fe53aa2e31d0a7cf284e894915
Drew DeVault <sir@cmpwn.com>
2012-09-05 23:09:02 -0600

    Missing files

---
+++ Craft.Net.Server/Packets/ChangeGameStatePacket.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Craft.Net.Data;
+
+namespace Craft.Net.Server.Packets
+{
+    public enum GameState
+    {
+        InvalidBed = 0, // wat
+        BeginRaining = 1,
+        EndRaining = 2,
+        ChangeGameMode = 3,
+        EnterCredits = 4
+    }
+
+    public class ChangeGameStatePacket : Packet
+    {
+        public GameState GameState;
+        public GameMode GameMode;
+
+        public ChangeGameStatePacket()
+        {
+        }
+
+        public ChangeGameStatePacket(GameState gameState) : this(gameState, GameMode.Survival) { }
+
+        public ChangeGameStatePacket(GameState gameState, GameMode gameMode)
+        {
+            GameState = gameState;
+            GameMode = gameMode;
+        }
+
+        public override byte PacketId
+        {
+            get { return 0x46; }
+        }
+
+        public override int TryReadPacket(byte[] buffer, int length)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public override void HandlePacket(MinecraftServer server, MinecraftClient client)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public override void SendPacket(MinecraftServer server, MinecraftClient client)
+        {
+            byte[] payload = new byte[] { PacketId, (byte)GameState, (byte)GameMode };
+            client.SendData(payload);
+        }
+    }
+}
---
+++ Craft.Net.Server/Packets/SpawnPositionPacket.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Craft.Net.Data;
+
+namespace Craft.Net.Server.Packets
+{
+    public class SpawnPositionPacket : Packet
+    {
+        public Vector3 SpawnPosition;
+
+        public SpawnPositionPacket()
+        {
+        }
+
+        public SpawnPositionPacket(Vector3 spawnPosition)
+        {
+            SpawnPosition = spawnPosition;
+        }
+
+        public override byte PacketId
+        {
+            get { return 0x6; }
+        }
+
+        public override int TryReadPacket(byte[] buffer, int length)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public override void HandlePacket(MinecraftServer server, MinecraftClient client)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public override void SendPacket(MinecraftServer server, MinecraftClient client)
+        {
+            byte[] payload = new byte[] { PacketId }
+                .Concat(DataUtility.CreateInt32((int)SpawnPosition.X))
+                .Concat(DataUtility.CreateInt32((int)SpawnPosition.Y))
+                .Concat(DataUtility.CreateInt32((int)SpawnPosition.Z))
+                .ToArray();
+            client.SendData(payload);
+        }
+    }
+}
