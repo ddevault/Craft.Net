966ad87106945bd7b21315d785afc4cfd613c7cf
Drew DeVault <sir@cmpwn.com>
2012-12-07 10:28:47 -0700

    Added 12w49a items

---
+++ Craft.Net.Data/Items/FireworkStarItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Craft.Net.Data.Items
+{
+    public class FireworkStarItem : Item
+    {
+        public override ushort Id
+        {
+            get { return 402; }
+        }
+    }
+}
