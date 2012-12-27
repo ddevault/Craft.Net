6d3a670e33e92b65c69d5c506479aaffffd8ada1
Drew DeVault <sir@cmpwn.com>
2012-12-27 14:05:27 -0700

    Fixed item drop for clay blocks

--- Craft.Net.Data/Blocks/ClayBlock.cs
+++ Craft.Net.Data/Blocks/ClayBlock.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Craft.Net.Data.Items;
 
 namespace Craft.Net.Data.Blocks
 {
@@ -15,6 +16,12 @@ namespace Craft.Net.Data.Blocks
         public override double Hardness
         {
             get { return 0.6; }
+        }
+
+        public override bool GetDrop(Items.ToolItem tool, out ItemStack[] drop)
+        {
+            drop = new[] { new ItemStack(new ClayItem(), 4) };
+            return true;
         }
     }
 }
