1f85519441bec2864365262b587f68ddab93b53c
Drew DeVault <sir@cmpwn.com>
2012-12-30 16:58:06 -0700

    Added missing file

---
+++ Craft.Net.Data.Test/LightingTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Craft.Net.Data.Test
+{
+    [TestFixture]
+    public class LightingTest
+    {
+        [Test]
+        public void TestSkyLight()
+        {
+        }
+    }
+}
