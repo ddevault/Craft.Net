480fd3cc8adfb64ce7707c532470ac330dc1222f
Drew DeVault <sir@cmpwn.com>
2013-01-10 11:47:54 -0700

    Added nether quartz drop

--- Craft.Net.Data/Blocks/NetherQuartzOreBlock.cs
+++ Craft.Net.Data/Blocks/NetherQuartzOreBlock.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Craft.Net.Data.Items;
 
 namespace Craft.Net.Data.Blocks
 {
@@ -10,6 +11,12 @@ namespace Craft.Net.Data.Blocks
         public override short Id
         {
             get { return 153; }
+        }
+
+        public override bool GetDrop(ToolItem tool, out ItemStack[] drop)
+        {
+            drop = new[] { new ItemStack(new NetherQuartzItem()) };
+            return tool is PickaxeItem;
         }
     }
 }
