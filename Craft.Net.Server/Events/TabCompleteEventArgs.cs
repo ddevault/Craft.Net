5ddf97a7497e33c94230f4e0bb79d7d77663b350
Drew DeVault <sir@cmpwn.com>
2013-05-25 12:31:37 -0600

    Added tab complete event, closes #151

---
+++ Craft.Net.Server/Events/TabCompleteEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Craft.Net.Server.Events
+{
+    public class TabCompleteEventArgs : EventArgs
+    {
+        public TabCompleteEventArgs(string text)
+        {
+            Text = text;
+        }
+
+        public string Text { get; set; }
+    }
+}
--- Craft.Net.Server/Handlers/PacketHandlers.cs
+++ Craft.Net.Server/Handlers/PacketHandlers.cs
@@ -40,6 +40,7 @@ namespace Craft.Net.Server.Handlers
             MinecraftServer.RegisterPacketHandler(PluginMessagePacket.PacketId, PluginMessage);
             MinecraftServer.RegisterPacketHandler(ChatMessagePacket.PacketId, ChatMessage);
             MinecraftServer.RegisterPacketHandler(KeepAlivePacket.PacketId, KeepAlive);
+            MinecraftServer.RegisterPacketHandler(TabCompletePacket.PacketId, TabComplete);
         }
 
         public static void ServerListPing(MinecraftClient client, MinecraftServer server, IPacket _packet)
@@ -78,6 +79,15 @@ namespace Craft.Net.Server.Handlers
             // TODO: Confirm value validity
             client.LastKeepAlive = DateTime.Now;
             client.Ping = (short)(client.LastKeepAlive - client.LastKeepAliveSent).TotalMilliseconds;
+        }
+
+        public static void TabComplete(MinecraftClient client, MinecraftServer server, IPacket _packet)
+        {
+            var packet = (TabCompletePacket)_packet;
+            var eventArgs = new TabCompleteEventArgs(packet.Text);
+            server.OnTabComplete(eventArgs);
+            packet.Text = eventArgs.Text;
+            client.SendPacket(packet);
         }
 
         private static string GetPingValue(MinecraftServer server)
