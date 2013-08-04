3773b9ec8f61068bf7e922e94a6ac77892ff51a8
Drew DeVault <sir@cmpwn.com>
2013-08-04 15:18:39 -0600

    Switch to BouncyCastle for crypto on all platforms
    This removes the MONO_Debug and MONO_Release build configurations.

--- source/Craft.Net.Networking/AesStream.cs
+++ source/Craft.Net.Networking/AesStream.cs
@@ -4,60 +4,32 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.IO;
-#if MONO
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Engines;
 using Org.BouncyCastle.Crypto.Modes;
 using Org.BouncyCastle.Crypto.Parameters;
-#endif
 
 namespace Craft.Net.Networking
 {
     public class AesStream : Stream
     {
-#if MONO
         private BufferedBlockCipher encryptCipher { get; set; }
         private BufferedBlockCipher decryptCipher { get; set; }
-#else
-        private CryptoStream decryptStream { get; set; }
-        private CryptoStream encryptStream { get; set; }
-#endif
         internal byte[] Key { get; set; }
 
         public AesStream(Stream stream, byte[] key)
         {
             BaseStream = stream;
             Key = key;
-#if MONO
             encryptCipher = new BufferedBlockCipher(new CfbBlockCipher(new AesFastEngine(), 8));
             encryptCipher.Init(true, new ParametersWithIV(
                 new KeyParameter(key), key, 0, 16));
             decryptCipher = new BufferedBlockCipher(new CfbBlockCipher(new AesFastEngine(), 8));
             decryptCipher.Init(false, new ParametersWithIV(
                 new KeyParameter(key), key, 0, 16));
-#else
-            var rijndael = GenerateAES(key);
-            var encryptTransform = rijndael.CreateEncryptor();
-            var decryptTransform = rijndael.CreateDecryptor();
-            encryptStream = new CryptoStream(BaseStream, encryptTransform, CryptoStreamMode.Write);
-            decryptStream = new CryptoStream(BaseStream, decryptTransform, CryptoStreamMode.Read);
-#endif
         }
 
         public Stream BaseStream { get; set; }
-
-#if !MONO
-        private static Rijndael GenerateAES(byte[] key)
-        {
-            var cipher = new RijndaelManaged();
-            cipher.Mode = CipherMode.CFB;
-            cipher.Padding = PaddingMode.None;
-            cipher.KeySize = 128;
-            cipher.FeedbackSize = 8;
-            cipher.Key = cipher.IV = key;
-            return cipher;
-        }
-#endif
 
         public override bool CanRead
         {
@@ -92,25 +64,17 @@ namespace Craft.Net.Networking
 
         public override int ReadByte()
         {
-#if MONO
             int value = BaseStream.ReadByte();
             if (value == -1) return value;
             return decryptCipher.ProcessByte((byte)value)[0];
-#else
-            return decryptStream.ReadByte();
-#endif
         }
 
         public override int Read(byte[] buffer, int offset, int count)
         {
-#if MONO
             int length = BaseStream.Read(buffer, offset, count);
             var decrypted = decryptCipher.ProcessBytes(buffer, offset, count);
             Array.Copy(decrypted, offset, buffer, offset, count);
             return length;
-#else
-            return decryptStream.Read(buffer, offset, count);
-#endif
         }
 
         public override long Seek(long offset, SeekOrigin origin)
@@ -125,45 +89,13 @@ namespace Craft.Net.Networking
 
         public override void Write(byte[] buffer, int offset, int count)
         {
-#if MONO
             var encrypted = encryptCipher.ProcessBytes(buffer, offset, count);
             BaseStream.Write(encrypted, 0, encrypted.Length);
-#else
-            encryptStream.Write(buffer, offset, count);
-#endif
         }
 
         public override void Close()
         {
-#if MONO
             BaseStream.Close();
-#else
-            decryptStream.Close();
-            encryptStream.Close();
-            BaseStream.Close();
-#endif
         }
-
-#if !MONO // TODO
-        public override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback callback, object state)
-        {
-            return decryptStream.BeginRead(buffer, offset, count, callback, state);
-        }
-
-        public override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback callback, object state)
-        {
-            return encryptStream.BeginWrite(buffer, offset, count, callback, state);
-        }
-
-        public override int EndRead(IAsyncResult asyncResult)
-        {
-            return decryptStream.EndRead(asyncResult);
-        }
-
-        public override void EndWrite(IAsyncResult asyncResult)
-        {
-            encryptStream.EndWrite(asyncResult);
-        }
-#endif
     }
 }
