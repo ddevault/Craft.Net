af4f0269dd0e6b7d6ebb03a441dc71da15492ef1
tuomas56 <pigworts2@gmail.com>
2013-09-21 11:42:02 +0100

    fixed trees, added biomes, removed unused methods
    Fixed random leaf spawning by preventing trees from spawning on the edge
    of the chunk which was causing them to warp the leaves to the other side
    of the chunk. added biomes based on height (below 10 above water level =
    beach / 5 above beach = extreme hills edge / above that = extreme hills)
    removed unused methods in noise.cs

--- source/Craft.Net.TerrainGeneration/Noise.cs
+++ source/Craft.Net.TerrainGeneration/Noise.cs
@@ -6,7 +6,8 @@ using Microsoft.Xna.Framework;
 
 namespace Craft.Net.TerrainGeneration
 {
-	//From GitHub repo
+	//From GitHub repo https://github.com/marbsydo/PerlinNoise
+	//Removed not needed methods
 
 	class Noise
 	{
@@ -45,21 +46,6 @@ namespace Craft.Net.TerrainGeneration
 			return total;
 		}
 
-		public double Get3D(double x, double y, double z)
-		{
-			double total = 0;
-			double tempAmplitude = amplitude;
-			double tempFrequency = frequency;
-			for (int i = 0; i < octaves; i++)
-			{
-				//tempFrequency = Math.Pow(2,i);
-				//double tempAmplitude = Math.Pow(persistence,i);
-				total += Gradient3D(x * tempFrequency, y * tempFrequency, z * tempFrequency, randomSeed) * tempAmplitude;
-				tempAmplitude *= persistence;
-				tempFrequency *= 2;
-			}
-			return total;
-		}
 
 		/// ***** \\\
 		/// NOISE \\\
@@ -67,12 +53,6 @@ namespace Craft.Net.TerrainGeneration
 
 		// Returns a random number based upon the number it is given
 		// The big numbers are prime numbers and can be changed to other primes
-		public double PerlinNoise1D(int x, int seed)
-		{
-			int n = (x * WorldGenConst.NOISE_MAGIC_X + WorldGenConst.NOISE_MAGIC_SEED * seed) & 0x7fffffff;
-			n = (n << 13) ^ n;
-			return (1.0 - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0);
-		}
 
 		public double PerlinNoise2D(int x, int y, int seed)
 		{
@@ -81,41 +61,7 @@ namespace Craft.Net.TerrainGeneration
 			return (1.0 - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0);
 		}
 
-		public double PerlinNoise3D(int x, int y, int z, int seed)
-		{
-			int n = (x * WorldGenConst.NOISE_MAGIC_X + y * WorldGenConst.NOISE_MAGIC_Y + z * WorldGenConst.NOISE_MAGIC_Z * WorldGenConst.NOISE_MAGIC_SEED * seed) & 0x7fffffff;
-			n = (n << 13) ^ n;
-			return (1.0 - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0);
-		}
 
-		/// ************** \\\
-		/// SEAMLESS NOISE \\\
-		/// ************** \\\
-		/// NOTE: These functions are WIP
-
-		public double NoiseSeamlessTest2D(int x, int y, int seed)
-		{
-			//return NoiseSeamless2D(x, y, seed, 1, 1);
-			//return NoiseSeamlessX2D(x, y, seed, 16);
-			return 0;
-		}
-
-		// Make noise seamless in X and Y direction, wrapping at w and h
-		public double NoiseSeamless2D(int x, int y, int seed, int w, int h)
-		{
-			double total = 0;
-			total += PerlinNoise2D(x, y, seed) * (w - x) * (h - y);
-			total += PerlinNoise2D(x - w, y, seed) * (x) * (h - y);
-			total += PerlinNoise2D(x - w, y - h, seed) * (x) * (y);
-			total += PerlinNoise2D(x, y - h, seed) * (w - x) * (y);
-			return total / (w * h);
-		}
-
-		// Make noise seamless in just the X direction, wrapping at w
-		public double NoiseSeamlessX2D(int x, int y, int seed, int w)
-		{
-			return ((w - x) * PerlinNoise2D(x, y, seed) + (x) * PerlinNoise2D(x - w, y, seed)) / w;
-		}
 
 		/// ******** \\\
 		/// GRADIENT \\\
@@ -135,55 +81,17 @@ namespace Craft.Net.TerrainGeneration
 			double v3 = Smooth2D(integerX, integerY + 1, seed);
 			double v4 = Smooth2D(integerX + 1, integerY + 1, seed);
 			// Interpolate X
-			double i1 = InterpolateCosine(v1, v2, fractionalX);
-			double i2 = InterpolateCosine(v3, v4, fractionalX);
+			double i1 = InterpolateCosine(v1, v2,fractionalX);
+			double i2 = InterpolateCosine(v3, v4,fractionalX);
 			// Interpolate Y
 			return InterpolateCosine(i1, i2, fractionalY);
 		}
 
-		public double Gradient3D(double x, double y, double z, int seed)
-		{
-			// Calculate integer coordinates
-			int integerX = (x > 0.0 ? (int)x : (int)x - 1);
-			int integerY = (y > 0.0 ? (int)y : (int)y - 1);
-			int integerZ = (z > 0.0 ? (int)z : (int)z - 1);
-			// Calculate remainder of coordinates
-			double fractionalX = x - (double)integerX;
-			double fractionalY = y - (double)integerY;
-			double fractionalZ = z - (double)integerZ;
-			// Get values for corners
-			double v1 = Smooth3D(integerX, integerY, integerZ, seed);
-			double v2 = Smooth3D(integerX + 1, integerY, integerZ, seed);
-			double v3 = Smooth3D(integerX, integerY + 1, integerZ, seed);
-			double v4 = Smooth3D(integerX + 1, integerY + 1, integerZ, seed);
-			double v5 = Smooth3D(integerX, integerY, integerZ + 1, seed);
-			double v6 = Smooth3D(integerX + 1, integerY, integerZ + 1, seed);
-			double v7 = Smooth3D(integerX, integerY + 1, integerZ + 1, seed);
-			double v8 = Smooth3D(integerX + 1, integerY + 1, integerZ + 1, seed);
-			// Interpolate X
-			double ii1 = InterpolateCosine(v1, v2, fractionalX);
-			double ii2 = InterpolateCosine(v3, v4, fractionalX);
-			double ii3 = InterpolateCosine(v5, v6, fractionalX);
-			double ii4 = InterpolateCosine(v7, v8, fractionalX);
-			// Interpolate Y
-			double i1 = InterpolateCosine(ii1, ii2, fractionalY);
-			double i2 = InterpolateCosine(ii3, ii4, fractionalY);
-			// Interpolate Z
-			return InterpolateCosine(i1, i2, fractionalZ);
-		}
 
 		/// ************* \\\
 		/// INTERPOLATING \\\
 		/// ************* \\\
 
-		// Interpolate linearly between two points, a and b, based upon the value x (which is between 0 and 1)
-		// When x is 0, a is returned
-		// When x is 1, b is returned
-		// Linear interpolation is the fastest but has most jagged output
-		public double InterpolateLinear(double a, double b, double x)
-		{
-			return a * (1 - x) + b * x;
-		}
 
 		// Slightly slower than linear interpolation but much smoother
 		public double InterpolateCosine(double a, double b, double x)
@@ -193,28 +101,10 @@ namespace Craft.Net.TerrainGeneration
 			return a * (1 - f) + b * f;
 		}
 
-		// Much slower than cosine and linear interpolation, but very smooth
-		// v1 = a, v2 = b
-		// v0 = point before a, v3 = point after b
-		public double InterpolateCubic(double v0, double v1, double v2, double v3, float x)
-		{
-			double p = (v3 - v2) - (v0 - v1);
-			double q = (v0 - v1) - p;
-			double r = v2 - v0;
-			double s = v1;
-			return p * x * x * x + q * x * x + r * x + s;
-		}
-
 		/// ********* \\\
 		/// SMOOTHING \\\
 		/// ********* \\\
 
-		public double Smooth1D(int x, int seed)
-		{
-			// 1/4 + 1/4 + 1/2 = 1;
-
-			return PerlinNoise1D(x, seed) / 2 + PerlinNoise1D(x - 1, seed) / 4 + PerlinNoise1D(x + 1, seed) / 4;
-		}
 
 		public double Smooth2D(int x, int y, int seed)
 		{
@@ -229,31 +119,7 @@ namespace Craft.Net.TerrainGeneration
 			return corners + sides + center;
 		}
 
-		public double Smooth3D(int x, int y, int z, int seed)
-		{
-			// 12/48 + 8/32 + 6/16 + 1/8 = 1
 
-			// ++0 -+0 0++ 0+-   +-0 --0 0-+ 0--   +0+ +0- -0+ -0-
-			double edges = 0;
-			edges += PerlinNoise3D(x + 1, y + 1, z, seed) + PerlinNoise3D(x - 1, y + 1, z, seed) + PerlinNoise3D(x, y + 1, z + 1, seed) + PerlinNoise3D(x, y + 1, z - 1, seed);
-			edges += PerlinNoise3D(x + 1, y - 1, z, seed) + PerlinNoise3D(x - 1, y - 1, z, seed) + PerlinNoise3D(x, y - 1, z + 1, seed) + PerlinNoise3D(x, y - 1, z - 1, seed);
-			edges += PerlinNoise3D(x + 1, y, z + 1, seed) + PerlinNoise3D(x + 1, y, z - 1, seed) + PerlinNoise3D(x - 1, y, z + 1, seed) + PerlinNoise3D(x - 1, y, z - 1, seed);
-			edges /= 48;
-			// --- --+ -+- -++ +-- +-+ ++- +++
-			double corners = 0;
-			corners += PerlinNoise3D(x - 1, y - 1, z - 1, seed) + PerlinNoise3D(x - 1, y - 1, z + 1, seed) + PerlinNoise3D(x - 1, y + 1, z - 1, seed) + PerlinNoise3D(x - 1, y + 1, z + 1, seed);
-			corners += PerlinNoise3D(x + 1, y - 1, z - 1, seed) + PerlinNoise3D(x + 1, y - 1, z + 1, seed) + PerlinNoise3D(x + 1, y + 1, z - 1, seed) + PerlinNoise3D(x + 1, y + 1, z + 1, seed);
-			corners /= 32;
-			// +00 -00 0+0 0-0 00+ 00-
-			double sides = 0;
-			corners += PerlinNoise3D(x - 1, y, z, seed) + PerlinNoise3D(x - 1, y, z, seed) + PerlinNoise3D(x, y + 1, z, seed);
-			corners += PerlinNoise3D(x, y - 1, z, seed) + PerlinNoise3D(x, y, z + 1, seed) + PerlinNoise3D(x, y, z - 1, seed);
-			corners /= 16;
-			// 000
-			double center = PerlinNoise3D(x, y, z, seed) / 8;
-
-			return corners + sides + center;
-		}
 	}
 
 	public static class WorldGenConst
--- source/Craft.Net.TerrainGeneration/StandardGenerator.cs
+++ source/Craft.Net.TerrainGeneration/StandardGenerator.cs
@@ -14,16 +14,28 @@ namespace Craft.Net.TerrainGeneration
         public string GeneratorOptions { get; set; }
         public long Seed { get; set; }
         public Vector3 SpawnPoint { get; set; }
-
+		/// <summary>
+		/// The noise generator used
+		/// </summary>
 		private Noise noise;
-
+		/// <summary>
+		/// The water level.
+		/// </summary>
 		private int waterLevel = 20;
 
 
-
+		/// <summary>
+		/// Generates a chunk by getting an array of heights then placing blocks of varying types up to that height
+		/// then it adds trees (leaves first then trunk)
+		/// 
+		/// </summary>
+		/// <returns>The chunk.</returns>
+		/// <param name="position">Position.</param>
+		//TODO:Add Ores
+		//TODO:Add Caves
         public Chunk GenerateChunk(Coordinates2D position)
 		{
-			int trees = new Random ().Next (0, 4);
+			int trees = new Random ().Next (0, 10);
 			int[,] heights = new int[16,16];
 			int[,] treeBasePositions = new int[trees,2];
 
@@ -33,81 +45,104 @@ namespace Craft.Net.TerrainGeneration
 				treeBasePositions [t, 1] = new Random ().Next (1, 16);
 			}
 
+			//Make a new Chunk
+			Chunk c = new Chunk (position);
 
-            //Make a new Chunk
-            Chunk c = new Chunk(position);
-            //Loop through all the blocks in the chunk
-            for (int x = 0; x < 16; x++)
-            {
-                for (int z = 0; z < 16; z++)
-                {
-					int h = -1 * (int)noise.Get2D (c.X * 16 + x, c.Z * 16 + z);
-					if (h <= 0)
-						h = h * -1 + 4;
-                    for (int y = 0; y < h; y++)
-                    {
 
-						if (y == 0)
+				//Loop through all the blocks in the chunk
+				for (int x = 0; x < 16; x++) {
+					for (int z = 0; z < 16; z++) {
+						int h = -1 * (int)noise.Get2D (c.X * 16 + x, c.Z * 16 + z);
+						if (h <= 0)
+							h = h * -1 + 4;
+						for (int y = 0; y < h; y++) {
+
+							if (y == 0)
                             //if at the bottom then set block to bedrock
-							c.SetBlockId (new Coordinates3D (x, y, z), 7);
-						else if (y < h - 1)
-                            //if not at the top set the block to dirt
-							c.SetBlockId (new Coordinates3D (x, y, z), 3);
-						else if (y < waterLevel)
+								c.SetBlockId (new Coordinates3D (x, y, z), 7);
+							else if (y < h - 1)
+                            //if not at the top set the block to dirt or stone depending on height
+							if (!(y < (h / 4) * 3))
+								c.SetBlockId (new Coordinates3D (x, y, z), 3);
+							else
+								c.SetBlockId (new Coordinates3D (x, y, z), 1);
+							else if (y < waterLevel)
 							//if below the water set to sand or clay
-						if (new Random ().Next (1, 40) < 5 && y < waterLevel - 4)
-							c.SetBlockId (new Coordinates3D (x, y, z), 82);
-						else
-							c.SetBlockId (new Coordinates3D (x, y, z), 12);
-						else
+							if (new Random ().Next (1, 40) < 5 && y < waterLevel - 4)
+								c.SetBlockId (new Coordinates3D (x, y, z), 82);
+							else
+								c.SetBlockId (new Coordinates3D (x, y, z), 12);
+							else
                             //otherwise set the block to grass or gravel rarely
 							if (new Random ().Next (1, 200) == 1)
-							c.SetBlockId (new Coordinates3D (x, y, z), 13);
-						else
-							c.SetBlockId (new Coordinates3D (x, y, z), 2);
+								c.SetBlockId (new Coordinates3D (x, y, z), 13);
+							else
+								c.SetBlockId (new Coordinates3D (x, y, z), 2);
 
 
-                    }
+						c.SetBiome ((byte)x, (byte)z, Biome.ExtremeHills);
+						if (y < waterLevel + 17)
+							c.SetBiome ((byte)x, (byte)z, Biome.ExtremeHillsEdge);
+						if (y < waterLevel + 10)
+							c.SetBiome ((byte)x, (byte)z, Biome.Beach);
 
-					heights [x,z] = h;
 
 
-					//create beaches and place water
-					if (h <= waterLevel) {
-						for (int w = 0; w < waterLevel - 3; w++) {
-							if (c.GetBlockId(new Coordinates3D(x,w,z)) == 0){
-								c.SetBlockId(new Coordinates3D(x,w,z), 8);
+						}
+
+						heights [x, z] = h;
+
+
+						//create beaches and place water
+						if (h <= waterLevel) {
+							for (int w = 0; w < waterLevel - 3; w++) {
+								if (c.GetBlockId (new Coordinates3D (x, w, z)) == 0) {
+									c.SetBlockId (new Coordinates3D (x, w, z), 8);
+								}
 							}
 						}
-					}
 
 
+						//generate colour of the wood and leaves
+						int woodcolour = new Random ().Next (1, 3);
+						if (woodcolour == 1)
+							woodcolour = 0;
 
-					//generate trees
-					for (int pos = 0; pos < trees; pos++) {
-						int random = new Random().Next (3, 4);
-						int height = heights[treeBasePositions [pos, 0] , treeBasePositions [pos, 1]];
-						if (height < waterLevel + 10)
-							break;
-						for (int t = 0; t <= random; t++) {
-							c.SetBlockId (new Coordinates3D (treeBasePositions [pos, 0], height + t, treeBasePositions [pos, 1]), 17);
+
+						//generate trees
+						for (int pos = 0; pos < trees; pos++) {
+							int random = new Random ().Next (3, 4);
+							int height = heights [treeBasePositions [pos, 0], treeBasePositions [pos, 1]];
+							if (treeBasePositions [pos, 0] < 14 && treeBasePositions [pos, 0] > 4 && treeBasePositions [pos, 1] < 14 && treeBasePositions [pos, 1] > 4) {
+								if (height < waterLevel + 10)
+									break;
+						
+
+
+								int leafwidth = 4;
+								for (int layer = 0; layer <= height; layer++) {
+									for (int w = 0; w <= leafwidth; w++) {
+										for (int l = 0; l <= leafwidth; l++) {
+											c.SetBlockId (new Coordinates3D (treeBasePositions [pos, 0] - (leafwidth / 2) + w, height + layer + random, treeBasePositions [pos, 1] - (leafwidth / 2) + l), 18);
+											c.SetMetadata (new Coordinates3D (treeBasePositions [pos, 0] - (leafwidth / 2) + w, height + layer + random, treeBasePositions [pos, 1] - (leafwidth / 2) + l), (byte)woodcolour);
+										}
+									}
+									leafwidth -= 1;
+								}
+
+								for (int t = 0; t <= (random + 2); t++) {
+									c.SetBlockId (new Coordinates3D (treeBasePositions [pos, 0], height + t, treeBasePositions [pos, 1]), 17);
+									c.SetMetadata (new Coordinates3D (treeBasePositions [pos, 0], height + t, treeBasePositions [pos, 1]), (byte)woodcolour);
+								}
+							}
+
 						}
 
-						int leafwidth = 4;
-						for (int layer = 0; layer <= height; layer++) {
-							for (int w = 0; w <= leafwidth; w++) {
-								for (int l = 0; l <= leafwidth; l++) {
-									c.SetBlockId (new Coordinates3D (treeBasePositions[pos,0] - (leafwidth/2) + w, height + layer + random,treeBasePositions[pos,1] - (leafwidth/2) + l), 18);
-								}							}
-								leafwidth -= 1;
-						}
 
 					}
+				
 
-
-                }
-            }
-
+				}
 
 
 
@@ -124,11 +159,12 @@ namespace Craft.Net.TerrainGeneration
 			if(Seed.Equals(new long()))
 				noise = new Noise (1, 0.01, 156, 2, new Random(new Random(new Random().Next()).Next()).Next());
 			else
-				noise = new Noise (1, 0.02, 156, 2, (int)Seed);
+				noise = new Noise (1, 0.01, 156, 2, (int)Seed);
+
+
 
         }
 	
-
 		private int RandRange(Random r, int rMin, int rMax)
 		{
 			return rMin + r.Next() * (rMax - rMin);
@@ -144,11 +180,7 @@ namespace Craft.Net.TerrainGeneration
 			return rMin + (float)r.NextDouble() * (rMax - rMin);
 		}
 
-		// Returns true if a is a power of 2, else false
-		private bool pow2(int a)
-		{
-			return (a & (a - 1)) == 0;
-		}
+
 
 	
     }
