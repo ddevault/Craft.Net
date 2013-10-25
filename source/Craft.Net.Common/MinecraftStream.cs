244ad1e3232e529ec1ca9d11097634bb8ff5352d
Drew DeVault <sir@cmpwn.com>
2013-10-25 15:52:24 -0600

    Add VarInt methods to MinecraftStream

--- source/Craft.Net.Common/MinecraftStream.cs
+++ source/Craft.Net.Common/MinecraftStream.cs
@@ -16,6 +16,37 @@ namespace Craft.Net.Common
 
         public static Encoding StringEncoding;
 
+        public long ReadVarInt()
+        {
+            ulong result = 0;
+            int shift = 0;
+            while (true)
+            {
+                byte current = ReadUInt8();
+                result |= (current & 0x7FUL) << shift++ * 7;
+                if (shift > 5)
+                    throw new InvalidDataException("VarInt may not be longer than 60 bits.");
+                if ((current & 0x80) != 128)
+                    break;
+            }
+            return (long)result;
+        }
+
+        public void WriteVarInt(long _value)
+        {
+            ulong value = (ulong)_value;
+            while (true)
+            {
+                if ((value & 0xFF) == 0)
+                {
+                    WriteUInt8((byte)value);
+                    break;
+                }
+                WriteUInt8((byte)(value & 0x7F | 0x80));
+                value >>= 7;
+            }
+        }
+
         public byte ReadUInt8()
         {
             int value = BaseStream.ReadByte();
