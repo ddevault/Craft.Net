f12d8823968af1871148c4e7407385890770d170
bobtwinkles <srkoser+GitHub@gmail.com>
2014-03-26 17:38:36 -0400

    Add OffsetBy to BoundingBox

--- source/Craft.Net.Common/BoundingBox.cs
+++ source/Craft.Net.Common/BoundingBox.cs
@@ -93,6 +93,20 @@ namespace Craft.Net.Common
             return new BoundingBox(vector2, vector1);
         }
 
+        /// <summary>
+        /// Offsets this BoundingBox. Does not modify this object, but returns a new one
+        /// </summary>
+        /// <returns>
+        /// The offset bounding box.
+        /// </returns>
+        /// <param name='Offset'>
+        /// The offset.
+        /// </param>
+        public BoundingBox OffsetBy(Vector3 Offset)
+        {
+            return new BoundingBox(Min + Offset, Max + Offset);
+        }
+
         public Vector3[] GetCorners()
         {
             return new Vector3[]
