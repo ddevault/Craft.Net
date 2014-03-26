5fc96f1da4988ad315577de488eff0a83ac73b55
bobtwinkles <srkoser+GitHub@gmail.com>
2014-03-26 17:41:04 -0400

    Hook C.N.Client up to C.N.Physics so client code gets some dumb stuff handled for free

--- source/Craft.Net.Client/MinecraftClient.Properties.cs
+++ source/Craft.Net.Client/MinecraftClient.Properties.cs
@@ -8,7 +8,7 @@ using Craft.Net.Physics;
 
 namespace Craft.Net.Client
 {
-    public partial class MinecraftClient : IPhysicsEntity
+    public partial class MinecraftClient
     {
         // Private so that silly people won't try to modify it behind our backs - everything should go through the
         // Position and OnGround properties
@@ -23,30 +23,7 @@ namespace Craft.Net.Client
         public bool OnGround
         {
             get
-            {
-                // Make sure that all position data is in a consistant state.
-                lock (_positionLock) {
-                    if (_position.Y > Craft.Net.Anvil.World.Height || _position.Y < 0)
-                    {
-                        _onGround = false;
-                    } else
-                    {
-                        try
-                        {
-                            var feetPosition = new Vector3(_position.X, _position.Y - 1.62 - 1, _position.Z);
-                            var coordinates = new Coordinates3D((int)feetPosition.X, (int)feetPosition.Y, (int)feetPosition.Z);
-                            var blockBoundingBox = LogicManager.GetBoundingBox(World.GetBlockId(coordinates));
-                            _onGround = blockBoundingBox.HasValue && blockBoundingBox.Value.Contains(feetPosition - (Vector3)coordinates);
-                        } catch (ArgumentException)
-                        {
-                            //Sometimes the world isn't loaded when we want it to be, so we pretend we are on the ground to
-                            //prevent falling through the world
-                            _onGround = true;
-                        }
-                    }
-                }
-                return _onGround;
-            }
+            { return _onGround; }
             private set { _onGround = value; }
         }
 
@@ -56,54 +33,20 @@ namespace Craft.Net.Client
             {
                 Vector3 ret;
                 // Make sure that position data is in a consistent state.
-                lock(_positionLock) {
+                lock (_positionLock)
+                {
                     ret = _position;
                 }
                 return ret;
             }
             set
             {
-                // Should be safe since C# locks are reentrant
-                // This means that even if the thread has already locked the _positionLock (e.g. the physics thread
-                // does this) we are safe.
-                lock(_positionLock) {
+                lock (_positionLock)
+                {
+                    _positionChanged = _position != value;
                     _position = value;
                 }
             }
-        }
-
-        // The physics engine needs a few properties as well
-        private object _velocityLock = new object();
-        private Vector3 _velocity = new Vector3();
-        public Vector3 Velocity
-        {
-            get
-            {
-                Vector3 ret;
-                lock (_velocityLock)
-                {
-                    ret = _velocity;
-                }
-                return ret;
-            }
-
-            set
-            {
-                lock (_velocityLock)
-                {
-                    _velocity = value;
-                }
-            }
-        }
-
-        public float AccelerationDueToGravity
-        {
-            get { return 32.0f;}
-        }
-
-        public float Drag
-        {
-            get { return 0.1f;}
         }
 
         internal float _pitch;
