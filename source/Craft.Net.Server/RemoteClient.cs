230c2548cb71007928f6e4437983fab94554839c
Nirad <miljours@gmail.com>
2015-01-17 18:34:29 -0500

    Revert back to original

--- source/Craft.Net.Server/RemoteClient.cs
+++ source/Craft.Net.Server/RemoteClient.cs
@@ -183,8 +183,8 @@ namespace Craft.Net.Server
             {
                 var newChunks = new List<Coordinates2D>();
                 // TODO: find where view distance incrase in solo...
-                for (int x = -3; x < 3; x++)
-                    for (int z = -3; z < 3; z++)
+                for (int x = -Settings.ViewDistance; x < Settings.ViewDistance; x++)
+                    for (int z = -Settings.ViewDistance; z < Settings.ViewDistance; z++)
                     {
                         newChunks.Add(new Coordinates2D(
                             ((int)Entity.Position.X >> 4) + x,
